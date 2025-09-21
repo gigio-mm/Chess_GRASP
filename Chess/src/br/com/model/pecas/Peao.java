82619dfe03bc810e37d5cf2fa3fd467755f469a2
Samuel De Francesco <defrancescodev@gmail.com>
2025-09-21 09:16:02 -0300

    feat: Criei as classes de peça

---
+++ Chess/src/br/com/model/pecas/Peao.java
@@ -0,0 +1,7 @@
+package br.com.model.pecas;
+
+public class Peao extends Peca {
+    public Peao(String cor, int valor) {
+        super(cor, valor);
+    }
+}
---
+++ Chess/src/br/com/model/pecas/Torre.java
@@ -0,0 +1,7 @@
+package br.com.model.pecas;
+
+public class Torre extends Peca {
+    public Torre(String cor, int valor) {
+        super(cor, valor);
+    }
+}
