23f85577e5c236b71c94b8f32a9112c8c6260eb0
Samuel De Francesco <defrancescodev@gmail.com>
2025-09-22 17:44:58 -0300

    Implementei os métodos da classe tabuleiro e criei as instâncias das 32 peças

--- src/main/java/chess/model/Tabuleiro.java
+++ src/main/java/chess/model/Tabuleiro.java
@@ -1,6 +1,7 @@
 package chess.model;
 
-import chess.model.pieces.Peca;
+import chess.model.enums.Cor;
+import chess.model.pieces.*;
 
 public class Tabuleiro {
 
@@ -12,21 +13,46 @@ public class Tabuleiro {
     }
 
     public Peca getPeca(Posicao pos) {
-        // Lógica será implementada depois
-        return null;
+        return pecas[pos.getLinha()][pos.getColuna()];
     }
 
     public void colocarPeca(Peca peca, Posicao pos) {
-        // Lógica será implementada depois
+        pecas[pos.getLinha()][pos.getColuna()] = peca;
     }
 
     public Peca removerPeca(Posicao pos) {
-        // Lógica será implementada depois
-        return null;
+        Peca removida = pecas[pos.getLinha()][pos.getColuna()];
+        pecas[pos.getLinha()][pos.getColuna()] = null;
+        return removida;
     }
 
     private void iniciarPecasPadrao() {
-        // A lógica complexa de posicionar as 32 peças virá aqui depois.
+        pecas[0][0] = new Torre(Cor.BRANCO);
+        pecas[0][1] = new Cavalo(Cor.BRANCO);
+        pecas[0][2] = new Bispo(Cor.BRANCO);
+        pecas[0][3] = new Rainha(Cor.BRANCO);
+        pecas[0][4] = new Rei(Cor.BRANCO);
+        pecas[0][5] = new Bispo(Cor.BRANCO);
+        pecas[0][6] = new Cavalo(Cor.BRANCO);
+        pecas[0][7] = new Torre(Cor.BRANCO);
+
+        for (int i = 0; i < 8; i++) {
+            pecas[1][i] = new Peao(Cor.BRANCO);
+        }
+
+        pecas[7][0] = new Torre(Cor.PRETO);
+        pecas[7][1] = new Cavalo(Cor.PRETO);
+        pecas[7][2] = new Bispo(Cor.PRETO);
+        pecas[7][3] = new Rainha(Cor.PRETO);
+        pecas[7][4] = new Rei(Cor.PRETO);
+        pecas[7][5] = new Bispo(Cor.PRETO);
+        pecas[7][6] = new Cavalo(Cor.PRETO);
+        pecas[7][7] = new Torre(Cor.PRETO);
+
+        for (int i = 0; i < 8; i++) {
+            pecas[7][i] = new Peao(Cor.PRETO);
+        }
+
         System.out.println("Tabuleiro inicializado (lógica de peças pendente).");
     }
 }
